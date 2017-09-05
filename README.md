1ddd2d3d0c7534af91058fd869e73c715e856cc9
matyi <matyas.csorba@melkweg.hu>
2017-09-05 13:27:54 +0200

    added readme with source material

---
+++ README.md
@@ -0,0 +1,11 @@
+#Glass try on pod
+
+It is a simple `javascript` pod for my own use.
+
+##Getting started
+
+After you cloned this repo, you have to run it on your `localhost` or have an HTTPS because Chrome will not allowing to
+use your camera.
+
+For face recognition, I used the [clmtrack.js](https://github.com/auduno/clmtrackr) combined with `jquery`.
+ 
--- assets/js/script.js
+++ assets/js/script.js
@@ -34,84 +34,40 @@ var App = {
         var glasses = new Image();
         glasses.src= "../assets/sg.png";
 
-        function drawLoop() {
-            requestAnimationFrame(drawLoop);
-            cc.clearRect(0, 0, canvasInput.width, canvasInput.height);
-            ctracker.draw(canvasInput);
-
-        }
-        drawLoop();
 
         var positions;
-        var hasAnyData = false;
-        var glassWidth,
-            glassHeight;
 
-        //console.log(ctracker);
         function positionLoop() {
 
             cc.clearRect(0,0,400, 300);
             requestAnimationFrame(positionLoop);
             positions = ctracker.getCurrentPosition();
-            // positions = [[x_0, y_0], [x_1,y_1], ... ]
-            // do something with the positions ...
-            //positions[23]
-            var /*faceWidth = getWidth(positions[0][0],positions[14][0], positions[0][1], positions[14][1]),*/
-                /*eyeHeight = getHeight(positions[22][0],positions[41][0], positions[22][1], positions[41][1]),*/
-                xPos = positions[0][0],
-                yPos = positions[22][1];
-            if(positions[0][0] && !hasAnyData){
-                console.log('van');
-                glassWidth = getWidth(positions[0][0],positions[14][0], positions[0][1], positions[14][1]);
-                glassHeight = getHeight(positions[22][0],positions[41][0], positions[22][1], positions[41][1]);
-                console.log(glassWidth);
-                console.log(glassHeight);
-                hasAnyData = true;
-            }
 
-            cc.drawImage(glasses, xPos, yPos,glassWidth , 120);
+            var faceWidth = getWidth(positions[0][0],positions[14][0], positions[0][1], positions[14][1]),
+                eyeHeight = getHeight(positions[22][0],positions[41][0], positions[22][1], positions[41][1]),
+                xPos = positions[0][0]-5,
+                yPos = positions[20][1];
 
+            cc.drawImage(glasses, xPos, yPos,faceWidth +10 , eyeHeight+7.5);
 
 
 
         }
-        //positionLoop();
+        positionLoop();
 
         function getWidth(a1,b1,a2,b2){
             var leftNumb = Math.pow((b1)-(a1),2);
             var rightNumb = Math.pow((b2)-(a2),2);
 
-            return Math.sqrt((leftNumb - rightNumb));
+            return Math.sqrt((leftNumb + rightNumb));
         }
         function getHeight(a1,b1,a2,b2){
             var leftNumb = Math.pow((b1)-(a1),2);
             var rightNumb = Math.pow((b2)-(a2),2);
 
-            return Math.sqrt((leftNumb - rightNumb));
+            return Math.sqrt((leftNumb + rightNumb));
         }
 
-
-
-            /*console.log(positions);
-            console.log(positions[23]);
-            console.log(positions[23][0]);
-            console.log(positions[28]);*/
-
-            /*
-                pontok:
-                balszél: 23
-                jobbszél: 28
-
-                balszem teteje: 24
-                balszem teteje: 26
-
-
-                arc balszéle: 0
-                arc jobbszéle: 14
-
-                szemöldök alja: 22
-                orr közepe: 41
-            */
 
     }
 };
@@ -120,6 +76,4 @@ $(document).ready(function(){
    App.init();
 });
 
-//források
-/*https://github.com/auduno/clmtrackr*/
 
